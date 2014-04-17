a0d3685eaef1b9ad2586ba7733a5485d3ed079f1
frolow <ArtemFrolow@Artems-Mac.local>
2014-04-17 19:16:58 +0300

    skip view

---
+++ OpKnowte/OKInfoViewController.h
@@ -0,0 +1,14 @@
+//
+//  OKInfoViewController.h
+//  OpKnowte
+//
+//  Created by Artem Frolow on 4/17/14.
+//  Copyright (c) 2014 OpKnowte Corp. All rights reserved.
+//
+
+#import "OKBaseViewController.h"
+#import "OKAppDelegate.h"
+
+@interface OKInfoViewController : OKBaseViewController
+
+@end
