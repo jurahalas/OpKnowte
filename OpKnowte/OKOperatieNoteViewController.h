9c51b003e1294f8613344786886c524078f2ba22
Olegek <olegmytsouda@mail.ru>
2014-04-25 21:22:25 +0300

    workingWithSegmentController

---
+++ OpKnowte/OKOperatieNoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  OKOperatieNoteViewController.h
+//  OpKnowte
+//
+//  Created by Olegek on 25.04.14.
+//  Copyright (c) 2014 OpKnowte Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OKOperatieNoteViewController : UIViewController
+
+@end
---
+++ OpKnowte/OKOperatieNoteViewController.m
@@ -0,0 +1,49 @@
+//
+//  OKOperatieNoteViewController.m
+//  OpKnowte
+//
+//  Created by Olegek on 25.04.14.
+//  Copyright (c) 2014 OpKnowte Corp. All rights reserved.
+//
+
+#import "OKOperatieNoteViewController.h"
+
+@interface OKOperatieNoteViewController ()
+
+@end
+
+@implementation OKOperatieNoteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
