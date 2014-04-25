2c2de89f5b89252001c59c2d10d2dfdc65d03d94
Olegek <olegmytsouda@mail.ru>
2014-04-25 10:04:23 +0300

    saveView

---
+++ OpKnowte/OKSaveViewController.h
@@ -0,0 +1,14 @@
+//
+//  OKSaveViewController.h
+//  OpKnowte
+//
+//  Created by Olegek on 25.04.14.
+//  Copyright (c) 2014 OpKnowte Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OKBaseViewController.h"
+
+@interface OKSaveViewController : OKBaseViewController
+
+@end
