a867a29fc924e65a9811107f259be40e5c4b4436
Olegek <olegmytsouda@mail.ru>
2014-05-05 09:21:44 +0300

    need_to_merge_s

---
+++ OpKnowte/OKSurgeonViewController.h
@@ -0,0 +1,13 @@
+//
+//  OKSurgeonViewController.h
+//  OpKnowte
+//
+//  Created by Olegek on 02.05.14.
+//  Copyright (c) 2014 OpKnowte Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OKSurgeonViewController : UIViewController
+
+@end
