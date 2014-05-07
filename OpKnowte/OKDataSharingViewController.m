b61e656ecacd130da576c834ef67a4fb18c64382
Olegek <olegmytsouda@mail.ru>
2014-05-07 11:25:27 +0300

    data_sharing_alert

--- OpKnowte/OKDataSharingViewController.m
+++ OpKnowte/OKDataSharingViewController.m
@@ -9,10 +9,12 @@
 #import "OKDataSharingViewController.h"
 
 @interface OKDataSharingViewController ()
+@property (strong, nonatomic) IBOutlet UISwitch *dataShareSwitch;
 
 @end
 
 @implementation OKDataSharingViewController
+@synthesize dataShareSwitch;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,6 +30,22 @@
     [super viewDidLoad];
     [self.navigationController setNavigationBarHidden:NO animated:YES];
     [self addBottomTabBar];
+    
+    //[self.dataShareSwitch setOn:YES];
+}
+- (IBAction)swithButton:(id)sender {
+    
+    if (dataShareSwitch.on){
+        UIAlertView *customAlertView = [[UIAlertView alloc] initWithTitle:@"Confirm"
+                                                              message:@"Please confirm that you want to share your anonymous data."
+                                                             delegate:self
+                                                    cancelButtonTitle:@"Cancel"
+                                                    otherButtonTitles:@"Share Data", nil];
+    
+        customAlertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+        [customAlertView textFieldAtIndex:0].placeholder = @"Password";
+        [customAlertView show];
+   }
 }
 - (IBAction)backButton:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
