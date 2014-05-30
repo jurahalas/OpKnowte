6d8ea4c7dc043fd1797cea1708648c11904e3899
Olegek <olegmytsouda@mail.ru>
2014-05-30 14:52:25 +0300

    fix_detail summary

--- OpKnowte/OKSurgicalLogsManager.h
+++ OpKnowte/OKSurgicalLogsManager.h
@@ -13,5 +13,4 @@
 
 - (void)getSurgeonDatesByUserID:(NSString*) userID AndProcedureID:(NSString*) procedureID  handler:(void(^)(NSString *errorMsg, id dates))handler;
 - (void)getSurgeonPerformanceDataByUserID:(NSString*) userID ProcedureID:(NSString *)procedureID FromTime:(NSString *)fromDate ToTime:(NSString *)toDate FromRecordNum:(NSString *)fromCase ToRecordNum:(NSString *)toCase  handler:(void(^)(NSString *errorMsg, NSMutableArray *dataArray))handler;
-
 @end
