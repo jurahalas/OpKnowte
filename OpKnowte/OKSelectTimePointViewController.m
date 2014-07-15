75d8d53d45de933fb6aeda0fab7494c455bc0e46
frolow <ArtemFrolow@Artems-Mac.local>
2014-07-15 22:15:55 +0300

    fixed PSA for ongoing 1 proc

--- OpKnowte/OKSelectTimePointViewController.m
+++ OpKnowte/OKSelectTimePointViewController.m
@@ -48,13 +48,6 @@
     
     self.caseObj = [OKCaseManager instance].selectedCase;
     
-    NSMutableArray *array = [[NSMutableArray alloc] init];
-//    for (int i=0; i<_totalSurgeonCases.count; i++) {
-//        OKLRRadicalProstatectomyModel *shockwaveCase = (OKLRRadicalProstatectomyModel*)_totalSurgeonCases[i];
-//        [array addObject:shockwaveCase.DetailID];
-//        self.selectedCases = [array componentsJoinedByString:@" "];
-//    }
-    
     [[OKLoadingViewController instance] showWithText:@"Loading..."];
     OKTimePointsManager *timePointsManager = [OKTimePointsManager instance];
     [timePointsManager getAllTimePointsWithProcID:_procID WithHandler:^(NSString *errorMsg, NSArray *timePointsArray) {
