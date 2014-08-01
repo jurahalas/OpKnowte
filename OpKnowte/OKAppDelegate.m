9fad2260b262592af0308a2d2bb7a581d061338c
fizzik <fozekoshzhenya@gmail.com>
2014-08-01 12:53:38 +0300

    remove hockeySDK

--- OpKnowte/OKAppDelegate.m
+++ OpKnowte/OKAppDelegate.m
@@ -35,13 +35,6 @@
     }
     
     [self restoreCurrentUser];
-    
-    
-    [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"e8f28c8ef6a8760eee6400b783d90a88"];
-    [[BITHockeyManager sharedHockeyManager] startManager];
-    [[BITHockeyManager sharedHockeyManager].authenticator authenticateInstallation];
-
-    
     return YES;
 }
 
